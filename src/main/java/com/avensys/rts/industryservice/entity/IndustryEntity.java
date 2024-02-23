1992519097ef40fd958b5036407394392cd8e570
rku-avensys <rahul@aven-sys.com>
2024-02-23 16:55:47 +0530

    removed @data annotation

--- src/main/java/com/avensys/rts/industryservice/entity/IndustryEntity.java
+++ src/main/java/com/avensys/rts/industryservice/entity/IndustryEntity.java
@@ -1,34 +1,44 @@
 package com.avensys.rts.industryservice.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /***
- * @author Koh He Xiang
- * This is the entity class for the industry table in the database
+ * @author Koh He Xiang This is the entity class for the industry table in the
+ *         database
  */
 @Entity
 @Table(name = "industry")
-@Data
+@Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class IndustryEntity {
 
-    @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY)
-    private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 
-    @Column(name = "name", length = 50, unique = true )
-    private String name;
+	@Column(name = "name", length = 50, unique = true)
+	private String name;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "parent_industry_id" , referencedColumnName = "id")
-    private IndustryEntity parentIndustry;
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "parent_industry_id", referencedColumnName = "id")
+	private IndustryEntity parentIndustry;
 
-    public IndustryEntity(String name, IndustryEntity parentIndustry) {
-        this.name = name;
-        this.parentIndustry = parentIndustry;
-    }
+	public IndustryEntity(String name, IndustryEntity parentIndustry) {
+		this.name = name;
+		this.parentIndustry = parentIndustry;
+	}
 }
--- src/main/java/com/avensys/rts/industryservice/payload/IndustryResponseDTO.java
+++ src/main/java/com/avensys/rts/industryservice/payload/IndustryResponseDTO.java
@@ -1,19 +1,21 @@
 package com.avensys.rts.industryservice.payload;
 
 import com.avensys.rts.industryservice.entity.IndustryEntity;
+
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
- * author: Koh He Xiang
- * This is the DTO class for the Industry response
+ * author: Koh He Xiang This is the DTO class for the Industry response
  */
-@Data
+@Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class IndustryResponseDTO {
-    private int id;
-    private String name;
-    private IndustryEntity industry;
+	private int id;
+	private String name;
+	private IndustryEntity industry;
 }
